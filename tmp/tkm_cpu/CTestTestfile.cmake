eaef29e6b0cc1a8e20e7d3f0f4eb61f3624d2afb
TomKlootwijk <ietsmeteentofzo@hotmail.com>
2026-08-22 11:36:09 +0200

    no message

---
+++ tmp/tkm_cpu/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Tom/Documents/theTomKlootwijkManifold/The_Tom_Klootwijk_Manifold_GPU/The_Tom_Klootwijk_Manifold_Bounded_Query_O1_Delivery_v3_0/implementation/Tom_Klootwijk_Bounded_Query_Substrate_v1.0.0
+# Build directory: C:/Users/Tom/Documents/theTomKlootwijkManifold/tmp/tkm_cpu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[tkm_bounded_query_tests]=] "C:/Users/Tom/Documents/theTomKlootwijkManifold/tmp/tkm_cpu/tkm_tests.exe")
+set_tests_properties([=[tkm_bounded_query_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Tom/Documents/theTomKlootwijkManifold/The_Tom_Klootwijk_Manifold_GPU/The_Tom_Klootwijk_Manifold_Bounded_Query_O1_Delivery_v3_0/implementation/Tom_Klootwijk_Bounded_Query_Substrate_v1.0.0/CMakeLists.txt;33;add_test;C:/Users/Tom/Documents/theTomKlootwijkManifold/The_Tom_Klootwijk_Manifold_GPU/The_Tom_Klootwijk_Manifold_Bounded_Query_O1_Delivery_v3_0/implementation/Tom_Klootwijk_Bounded_Query_Substrate_v1.0.0/CMakeLists.txt;0;")
